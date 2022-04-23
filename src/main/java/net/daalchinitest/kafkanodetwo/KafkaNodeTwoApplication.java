cb3c60cff8aad038f46ca28c67777845bdb75b80
Amar Poonia <amar.poonia@daalchini.co.in>
2022-04-23 16:49:42 +0530

    Initial commit

---
+++ src/main/java/net/daalchinitest/kafkanodetwo/KafkaNodeTwoApplication.java
@@ -0,0 +1,13 @@
+package net.daalchinitest.kafkanodetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNodeTwoApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(KafkaNodeTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/daalchinitest/kafkanodetwo/KafkaNodeTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.daalchinitest.kafkanodetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaNodeTwoApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
